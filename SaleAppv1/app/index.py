d5c01637cdd1fb0e72471d77d4e30ab4310e6f7d
Hoang <nguyenduyminhhoang@gmail.com>
2023-11-21 22:59:55 +0700

    update pages

--- SaleAppv1/app/index.py
+++ SaleAppv1/app/index.py
@@ -1,15 +1,45 @@
-from flask import render_template, request
+import math
+
+from flask import render_template, request, redirect
 import dao
-from app import app
+from app import app, login
+from flask_login import login_user
 
 
 @app.route("/")
 def index():
     kw = request.args.get('kw')
+
+    cate_id = request.args.get('cate_id')
+    page = request.args.get('page')
+
     cate = dao.load_categories()
-    products = dao.load_products(kw=kw)
-    return render_template("index.html", categories=cate, products=products)
+    products = dao.load_products(kw=kw, cate_id=cate_id, page=page)
+
+    num = dao.count_product()
+    page_size = app.config['PAGE_SIZE']
+
+    return render_template("index.html", categories=cate, products=products,
+                           pages=math.ceil(num/page_size))
+
+
+@app.route('/admin/login', methods=['post'])
+def admin_login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    user = dao.auth_user(username=username, password=password)
+    if user:
+        login_user(user=user)
+
+    return redirect('/admin')
+
+
+@login.user_loader
+def get_user(user_id):
+    return dao.get_user_by_id(user_id)
 
 
 if __name__ == "__main__":
+    from app import admin
+
     app.run(debug=True)
--- SaleAppv1/app/models.py
+++ SaleAppv1/app/models.py
@@ -1,6 +1,25 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 from app import db, app
+from flask_login import UserMixin
+import enum
+
+
+class UserRoleEnum(enum.Enum):
+    USER = 1
+    ADMIN = 2
+
+
+class User(db.Model,UserMixin):
+    id= Column(Integer,primary_key=True,autoincrement=True)
+    name= Column(String(50),nullable=False,unique=True)
+    username = Column(String(50),nullable=False,unique=True)
+    password=Column(String(100),nullable=False)
+    avatar = Column(String(100), default='https://static2-images.vnncdn.net/files/publish/2022/12/8/meo-1-1416.jpg')
+    user_role = Column(Enum(UserRoleEnum), default= UserRoleEnum.USER)
+
+    def __str__(self):
+        return self.name
 
 
 class Category(db.Model):
@@ -11,6 +30,10 @@ class Category(db.Model):
     products = relationship('Product',backref='category', lazy= True)
 
 
+    def __str__(self):
+        return self.name
+
+
 class Product(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False, unique=True)
@@ -19,26 +42,39 @@ class Product(db.Model):
     category_id = Column(Integer, ForeignKey(Category.id), nullable= False)
 
 
+    def __str__(self):
+        return self.name
+
+
 if __name__ == '__main__':
     with app.app_context():
-        # db.create_all()
+        db.create_all()
+        #
+        # import hashlib
+        #
+        # u = User(name='Admin', username='admin',
+        #          password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()),
+        #          user_role= UserRoleEnum.ADMIN)
+        # db.session.add(u)
+        # db.session.commit()
+
         # c1 = Category(name='Mobile')
         # c2 = Category(name='Tablet')
-        c3 = Category(name='Desktop')
-
+        # c3 = Category(name='Desktop')
+        #
         # db.session.add(c1)
         # db.session.add(c2)
-        db.session.add(c3)
+        # db.session.add(c3)
         # db.session.commit()
-        p1 = Product(name='iPhone 13', price=20000000, category_id=1,
-                     image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
-        p2 = Product(name='Galaxy S23 Plus', price=22000000, category_id=1,
-                     image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
-        p3 = Product(name='iPad Pro 2023', price=35000000, category_id=2,
-                     image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
-        p4 = Product(name='Galaxy Tab S9', price=24000000, category_id=2,
-                     image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
-        p5 = Product(name='Note 23', price=20000000, category_id=1,
-                     image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
-        db.session.add_all([p1, p2, p3, p4, p5])
-        db.session.commit()
+        # p1 = Product(name='iPhone 13', price=20000000, category_id=1,
+        #              image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
+        # p2 = Product(name='Galaxy S23 Plus', price=22000000, category_id=1,
+        #              image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
+        # p3 = Product(name='iPad Pro 2023', price=35000000, category_id=2,
+        #              image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
+        # p4 = Product(name='Galaxy Tab S9', price=24000000, category_id=2,
+        #              image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
+        # p5 = Product(name='Note 23', price=20000000, category_id=1,
+        #              image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
+        # db.session.add_all([p1, p2, p3, p4, p5])
+        # db.session.commit()
